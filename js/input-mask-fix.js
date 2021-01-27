970eb09e08f3c455f9d854c748ac8d00811136ea
Artem Dybin <tema.dybin@yandex.ru>
2021-01-27 15:43:18 +0300

    Add files via upload

---
+++ js/input-mask-fix.js
@@ -0,0 +1,15 @@
+$.fn.setCursorPosition = function(pos) {
+  if ($(this).get(0).setSelectionRange) {
+    $(this).get(0).setSelectionRange(pos, pos);
+  } else if ($(this).get(0).createTextRange) {
+    var range = $(this).get(0).createTextRange();
+    range.collapse(true);
+    range.moveEnd('character', pos);
+    range.moveStart('character', pos);
+    range.select();
+  }
+};
+
+$('input[type="tel"]').click(function(){
+    $(this).setCursorPosition(4);  // set position number
+  });
